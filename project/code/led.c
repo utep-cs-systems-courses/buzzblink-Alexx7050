cc612efc0b00bb756a433b380ba21e11dff4fdd2
Alejandro Jaramillo <60363190+Alexx7050@users.noreply.github.com>
2022-07-11 13:43:02 -0600

    Update led.c

--- project/code/led.c
+++ project/code/led.c
@@ -2,6 +2,11 @@
 #include "led.h"
 #include "switches.h"
 
+unsigned char red_on = 0, green_on = 0;
+unsigned char led_changed = 0;
+
+static char redVal[] = {0, LED_RED}, greenVal[] = {0, LED_GREEN};
+
 void led_init()
 {
   P1DIR |= LEDS;		// bits attached to leds are output
