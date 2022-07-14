3192ca769a8e7a85ed79b304e9dbf50746f3a662
Alejandro Jaramillo <60363190+Alexx7050@users.noreply.github.com>
2022-07-14 13:30:40 -0600

    Update led.c

--- project/code/led.c
+++ project/code/led.c
@@ -14,15 +14,13 @@ void led_init()
   led_update();
 }
 
-void led_update(){
-  if (switch_state_changed) {
-    char ledFlags = 0; /* by default, no LEDs on */
-
-    ledFlags |= switch_state_down ? LED_GREEN : 0;
-    ledFlags |= switch_state_down ? 0 : LED_RED;
-
-    P1OUT &= (0xff - LEDS) | ledFlags; // clear bits for off leds
-    P1OUT |= ledFlags;         // set bits for on leds
+void led_update()
+{
+  if (led_changed) {
+    char ledFlags = redVal[red_on] | greenVal[green_on];
+    
+    P1OUT &= (0xff^LEDS) | ledFlags; // clear bit for off leds
+    P1OUT |= ledFlags;		     // set bit for on leds
+    led_changed = 0;
   }
-  switch_state_changed = 0;
 }
