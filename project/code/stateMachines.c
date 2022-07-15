42bc1974e62e0d91d604f038d432a0779a766b47
Alejandro Jaramillo <60363190+Alexx7050@users.noreply.github.com>
2022-07-15 14:18:21 -0600

    Update stateMachines.c

--- project/code/stateMachines.c
+++ project/code/stateMachines.c
@@ -4,32 +4,32 @@
 #include "switches.h"
 #include "buzzer.h"
 
-// char toggle_red()		/* always toggle! */
-// {
-//   static char state = 0;
+char toggle_red()		/* always toggle! */
+{
+  static char state = 0;
 
-//   switch (state) {
-//   case 0:
-//     red_on = 1;
-//     state = 1;
-//     break;
-//   case 1:
-//     red_on = 0;
-//     state = 0;
-//     break;
-//   }
-//   return 1;			/* always changes an led */
-// }
+  switch (state) {
+  case 0:
+    red_on = 1;
+    state = 1;
+    break;
+  case 1:
+    red_on = 0;
+    state = 0;
+    break;
+  }
+  return 1;			/* always changes an led */
+}
 
-// char toggle_green()	/* only toggle green if red is on!  */
-// {
-//   char changed = 0;
-//   if (red_on) {
-//     green_on ^= 1;
-//     led_changed = 1;
-//   }
-//   return changed;
-// }
+char toggle_green()	/* only toggle green if red is on!  */
+{
+  char changed = 0;
+  if (red_on) {
+    green_on ^= 1;
+    led_changed = 1;
+  }
+  return changed;
+}
 
 void toggle_green(){
   red_on = 0;
