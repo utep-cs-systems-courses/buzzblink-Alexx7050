d2d3fe1f2abb5126a6768e410aef417fdc28ae9d
Alejandro Jaramillo <60363190+Alexx7050@users.noreply.github.com>
2022-07-15 14:14:38 -0600

    Update stateMachines.c

--- project/code/stateMachines.c
+++ project/code/stateMachines.c
@@ -4,31 +4,45 @@
 #include "switches.h"
 #include "buzzer.h"
 
-char toggle_red()		/* always toggle! */
-{
-  static char state = 0;
+// char toggle_red()		/* always toggle! */
+// {
+//   static char state = 0;
 
-  switch (state) {
-  case 0:
-    red_on = 1;
-    state = 1;
-    break;
-  case 1:
-    red_on = 0;
-    state = 0;
-    break;
-  }
-  return 1;			/* always changes an led */
+//   switch (state) {
+//   case 0:
+//     red_on = 1;
+//     state = 1;
+//     break;
+//   case 1:
+//     red_on = 0;
+//     state = 0;
+//     break;
+//   }
+//   return 1;			/* always changes an led */
+// }
+
+// char toggle_green()	/* only toggle green if red is on!  */
+// {
+//   char changed = 0;
+//   if (red_on) {
+//     green_on ^= 1;
+//     led_changed = 1;
+//   }
+//   return changed;
+// }
+
+void toggle_green(){
+  red_on = 0;
+  green_on = 1;
+  led_changed = 1;
+  led_update();
 }
 
-char toggle_green()	/* only toggle green if red is on!  */
-{
-  char changed = 0;
-  if (red_on) {
-    green_on ^= 1;
-    led_changed = 1;
-  }
-  return changed;
+void toggle_red(){
+  red_on = 1;
+  green_on = 0;
+  led_changed = 1;
+  led_update();
 }
 
 void turn_off(){
@@ -38,7 +52,7 @@ void turn_off(){
   led_update();
 }
 
-void state_advance()
+void state_advance(n_switch)
 {
  
  char n_switch;
