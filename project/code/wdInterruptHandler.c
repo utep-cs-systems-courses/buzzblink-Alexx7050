737f3956c40a6b20890eaba9a8643760777c9df5
Alejandro Jaramillo <60363190+Alexx7050@users.noreply.github.com>
2022-07-15 14:55:05 -0600

    Update wdInterruptHandler.c

--- project/code/wdInterruptHandler.c
+++ project/code/wdInterruptHandler.c
@@ -6,7 +6,8 @@
 void
 __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
   static char blink_count = 0;
-  if (++blink_count == 125) {
+  blink_count++;
+  if (blink_count == 250) {
     blink_count = 0;
     P1OUT ^= LED_GREEN;		/* toggle green LED */
     P1OUT ^= LED_RED;		/* toggle green LED */
