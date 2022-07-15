3db1646a7342480494cc0a04eebe053bb14c5aaa
Alejandro Jaramillo <60363190+Alexx7050@users.noreply.github.com>
2022-07-15 15:07:26 -0600

    Update wdInterruptHandler.c

--- project/code/wdInterruptHandler.c
+++ project/code/wdInterruptHandler.c
@@ -3,30 +3,9 @@
 #include "libTimer.h"
 #include "stateMachines.h"
 
-static char second_count = 0;
-static char led_state = 0; 
+  static char blink_count = 0;
 
-void
-__interrupt_vec(WDT_VECTOR) WDT()	/* 250 interrupts/sec */
-{
-  if (second_count >=250 )
-  {
-    second_count = 0;
-    change_led();
+  if(++blink_count == 250){
+    state_advance();
+    blink_count = 0;
   }
-  second_count++;
-}
-
-void change_led()
-{
-  switch(led_state){
-  case(0):
-    P1OUT |= LED_GREEN;   //turn on
-    led_state = 1;
-    break;
-  case(1):
-    P1OUT &= ~LED_GREEN;  //turn off
-    led_state = 0;
-    break;
-  }
-}
