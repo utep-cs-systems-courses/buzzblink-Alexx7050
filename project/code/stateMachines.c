a0b4e13823789f6fc6aa9f8e1448f58cbc869e30
Alejandro Jaramillo <60363190+Alexx7050@users.noreply.github.com>
2022-07-15 13:59:24 -0600

    Update stateMachines.c

--- project/code/stateMachines.c
+++ project/code/stateMachines.c
@@ -1,6 +1,8 @@
 #include <msp430.h>
 #include "stateMachines.h"
 #include "led.h"
+#include "switches.h"
+#include "buzzer.h"
 
 char toggle_red()		/* always toggle! */
 {
@@ -24,22 +26,34 @@ char toggle_green()	/* only toggle green if red is on!  */
   char changed = 0;
   if (red_on) {
     green_on ^= 1;
-    changed = 1;
+    led_changed = 1;
   }
   return changed;
 }
 
-
-void state_advance()		/* alternate between toggling red & green */
-{
-  char changed = 0;  
-
-  static enum {R=0, G=1} color = G;
-  switch (color) {
-  case R: changed = toggle_red(); color = G; break;
-  case G: changed = toggle_green(); color = R; break;
-  }
-
-  led_changed = changed;
+void turn_off(){
+  red_on = 0;
+  green_on = 0;
+  led_changed = 1;
   led_update();
+}
+
+void state_advance()
+{
+ switch(n_switch){
+  case 0:
+    toggle_red();
+    break;
+  case 1:
+    toggle_green();
+    break;
+  case 2:
+     toggle_red();
+     toggle_green();
+     buzzer_set_period(3432);
+    break;
+  case 3:
+    buzzer_set_period(0);
+    break;
+ } 
 }
