b2bfdef5cb58c70eb76e3ae6f0eeb8017cb84089
Alejandro Jaramillo <60363190+Alexx7050@users.noreply.github.com>
2022-07-18 15:29:32 -0600

    Update switches.c

--- project/code/switches.c
+++ project/code/switches.c
@@ -1,7 +1,7 @@
 #include <msp430.h>
 #include "switches.h"
 #include "led.h"
-#include "stateAdvance.h"
+#include "state_advance.h"
 
 char switch_state_down, switch_state_changed; /* effectively boolean */
 
