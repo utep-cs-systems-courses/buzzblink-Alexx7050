b5cc2c33aee9cb09361fd925476b10fba235fdba
Alejandro Jaramillo <60363190+Alexx7050@users.noreply.github.com>
2022-07-18 13:33:38 -0600

    Update wdInterruptHandler.c

--- project/code/wdInterruptHandler.c
+++ project/code/wdInterruptHandler.c
@@ -3,14 +3,28 @@
 #include "libTimer.h"
 #include "stateMachines.h"
 
-  static char blink_count = 0;
+// global state vars that control blinking
+int blinkLimit = 5;  // duty cycle = 1/blinkLimit
+int blinkCount = 0;  // cycles 0...blinkLimit-1
+int secondCount = 0; // state var representing repeating time 0…1s
 
 void
-__interrupt_vec(WDT_VECTOR) WDT(){
+__interrupt_vec(WDT_VECTOR) WDT()	/* 250 interrupts/sec */
+{
+  // handle blinking 
+  blinkCount ++;
+  if (blinkCount >= blinkLimit) { // on for 1 interrupt period
+    blinkCount = 0;
+    P1OUT |= LED_GREEN;
+  } else		          // off for blinkLimit - 1 interrupt periods
+    P1OUT &= ~LED_GREEN;
 
-  if(++blink_count == 250){
-    state_advance();
-    blink_count = 0;
+  // measure a second
+  secondCount ++;
+  if (secondCount >= 250) {  // once each second
+    secondCount = 0;
+    blinkLimit ++;	     // reduce duty cycle
+    if (blinkLimit >= 8)     // but don't let duty cycle go below 1/7.
+      blinkLimit = 0;
   }
-  
-}
+} 
