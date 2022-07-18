578bf4aba0846f03af7f38677d956b29cfac3145
Alejandro Jaramillo <60363190+Alexx7050@users.noreply.github.com>
2022-07-18 15:12:36 -0600

    Update stateAdvance-c.c

--- project/code/stateAdvance-c.c
+++ project/code/stateAdvance-c.c
@@ -9,19 +9,15 @@ void state_advance(n_switch)
  switch(n_switch){
   case 0:
     toggle_red();
-    buzzer_set_period(0);
     break;
   case 1:
     toggle_green();
-    buzzer_set_period(1000);
     break;
   case 2:
      toggle_both();
-     buzzer_set_period(6000);
     break;
   case 3:
     turn_off();
-    buzzer_set_period(0);
     break;
  } 
 }
