09830fb27b0655892b9b8905c35c1f43e45635b7
Alejandro Jaramillo <60363190+Alexx7050@users.noreply.github.com>
2022-07-18 15:27:52 -0600

    Update and rename stateAdvance-s.s to state_advance-s.s

---
+++ project/code/state_advance-s.s
@@ -0,0 +1,35 @@
+    .arch msp430g2553
+    .p2align 1,0
+
+    .text
+jt: .word default
+    .word case0
+    .word case1
+    .word case2
+    .word case3
+
+    .global state_advance
+    .extern n_switch
+
+state_advance:
+    ;; if (n_switch > 3) goto default
+    cmp &n_switch, #3
+    jnc default
+    ;; PC = jt[n_switch]
+    mov &n_switch, r12
+    add r12, r12
+    mov jt(r12), r0
+case0:
+    call #toggle_red()
+    jmp end
+case1:
+    call #toggle_green()
+    jmp end
+case2:
+    call #toggle_both()
+    jmp end
+case3:
+    call #turn_off()
+    jmp end
+end:
+    pop r0
