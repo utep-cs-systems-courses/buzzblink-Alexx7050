288c45773e67a7eb265e1eb570e847609a237b6f
Alejandro Jaramillo <60363190+Alexx7050@users.noreply.github.com>
2022-07-18 15:03:34 -0600

    Update stateMachines.c

--- project/code/stateMachines.c
+++ project/code/stateMachines.c
@@ -35,25 +35,3 @@ void turn_off(){
   led_changed = 1;
   led_update();
 }
-
-void state_advance(n_switch)
-{
- switch(n_switch){
-  case 0:
-    toggle_red();
-    buzzer_set_period(0);
-    break;
-  case 1:
-    toggle_green();
-    buzzer_set_period(1000);
-    break;
-  case 2:
-     toggle_both();
-     buzzer_set_period(6000);
-    break;
-  case 3:
-    turn_off();
-    buzzer_set_period(0);
-    break;
- } 
-}
